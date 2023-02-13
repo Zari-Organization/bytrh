a8cbcb005ce4725e3df80254a18c1f519ef69fbc
Nader <nadersalah20201111@gmail.com>
2023-02-13 15:35:27 +0200

    Phone and Country Dialog Arabic Language Added

--- lib/View/Screens/Splash_Screen/splash_screen.dart
+++ lib/View/Screens/Splash_Screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -40,7 +41,6 @@ class _SplashViewState extends State<SplashScreen> {
     if (kDebugMode) {
       log(GetStorage().read<String>('AccessToken').toString());
       log(GetStorage().read<bool>('onBoarding').toString());
-      // log(GetStorage().read<String>('userName').toString());
     }
     startDelay();
     super.initState();
